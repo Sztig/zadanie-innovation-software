b644332f124109910f22b4c99e2bc4c7b0a2f16c
Sebastian Jamuła <sebastianjamula@gmail.com>
2024-04-15 18:17:11 +0200

    added bundles (orm, http, maker) and currency entity

---
+++ src/Entity/Currency.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CurrencyRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Types\UuidType;
+use Symfony\Component\Uid\Uuid;
+
+#[ORM\Entity(repositoryClass: CurrencyRepository::class)]
+class Currency
+{
+    #[ORM\Id]
+    #[ORM\Column(type: UuidType::NAME, unique: true)]
+    #[ORM\GeneratedValue(strategy: 'CUSTOM')]
+    #[ORM\CustomIdGenerator(class: 'doctrine.uuid_generator')]
+    private ?Uuid $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $currency_code = null;
+
+    #[ORM\Column]
+    private ?float $exchange_rate = null;
+
+    public function getId(): ?Uuid
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getCurrencyCode(): ?string
+    {
+        return $this->currency_code;
+    }
+
+    public function setCurrencyCode(string $currency_code): static
+    {
+        $this->currency_code = $currency_code;
+
+        return $this;
+    }
+
+    public function getExchangeRate(): ?float
+    {
+        return $this->exchange_rate;
+    }
+
+    public function setExchangeRate(float $exchange_rate): static
+    {
+        $this->exchange_rate = $exchange_rate;
+
+        return $this;
+    }
+}
