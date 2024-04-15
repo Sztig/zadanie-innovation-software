f224f2a85ab97b7d1a6f6475a4e41e6e25193c96
Sebastian Jamuła <sebastianjamula@gmail.com>
2024-04-15 18:37:40 +0200

    added service for getting/updating currencies and a cli command for updating (app:update-exchange-rates)

---
+++ src/Command/UpdateExchangeRatesCommand.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Command;
+
+use App\Service\CurrencyExchangeService;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class UpdateExchangeRatesCommand extends Command
+{
+    protected static $defaultName = 'app:update-exchange-rates';
+
+    public function __construct(
+        private CurrencyExchangeService $currencyExchangeService
+    )
+    {
+        parent::__construct();
+    }
+
+    protected function configure()
+    {
+        $this
+            ->setName('app:update-exchange-rates');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $this->currencyExchangeService->updateCurrencies();
+
+        $output->writeln('Currency updated successfully!');
+
+        return Command::SUCCESS;
+    }
+}
---
+++ src/Service/CurrencyExchangeService.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Service;
+
+use App\Entity\Currency;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+
+class CurrencyExchangeService
+{
+    private string $url = 'https://api.nbp.pl/api/exchangerates/tables/a/';
+
+    public function __construct(
+        private EntityManagerInterface $entityManager,
+        private HttpClientInterface    $client
+    )
+    {
+    }
+
+    public function getCurrencies(): array
+    {
+        return $this->entityManager->getRepository(Currency::class)->findAll();
+    }
+
+    public function updateCurrencies(): void
+    {
+        $data = $this->getDataFromResponse();
+
+        if ($data) {
+            $this->updateStoredCurrencies($data);
+        }
+    }
+
+    private function getDataFromResponse(): array
+    {
+        $response = $this->client->request('GET', $this->url, [
+            'headers' => [
+                'Accept' => 'application/json',
+            ]
+        ]);
+
+        return $response->toArray();
+    }
+
+    private function updateStoredCurrencies(array $currencyData)
+    {
+        foreach ($currencyData[0]['rates'] as $exchangeRate) {
+            $currency = $this->entityManager->getRepository(Currency::class)->findOneBy([
+                'currency_code' => $exchangeRate['code']
+            ]);
+
+            if ($currency) {
+                $currency->setExchangeRate($exchangeRate['mid']);
+            } else {
+                $currency = new Currency();
+                $currency->setName($exchangeRate['currency']);
+                $currency->setExchangeRate((float)$exchangeRate['mid']);
+                $currency->setCurrencyCode($exchangeRate['code']);
+                $this->entityManager->persist($currency);
+            }
+        }
+
+        $this->entityManager->flush();
+    }
+}
